1ee4e827e051d9a318a9b1923cbe9578c88ab78e
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-03-22 16:55:25 -0400

    crew ai and agents folder

--- latest_ai_development/src/latest_ai_development/main.py
+++ latest_ai_development/src/latest_ai_development/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import sys
 import warnings
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from datetime import datetime
 
 from latest_ai_development.crew import LatestAiDevelopment
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
@@ -17,9 +21,12 @@ def run():
     """
     Run the crew.
     """
+    current_date = datetime.now()
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'topic': 'Trending topics on social media with a potential to go viral',
+        'current_year': str(current_date.year),
+        'current_date': current_date.strftime("%Y-%m-%d"),
+        'time_period': 'last 24 hours'
     }
     
     try:
@@ -33,7 +40,7 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        "topic": "AI LLMs"
+        "topic": "Trending topics on social media with a potential to go viral"
     }
     try:
         LatestAiDevelopment().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
@@ -56,10 +63,13 @@ def test():
     Test the crew execution and returns the results.
     """
     inputs = {
-        "topic": "AI LLMs"
+        "topic": "Trending topics on social media with a potential to go viral"
     }
     try:
         LatestAiDevelopment().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while testing the crew: {e}")
+
+if __name__ == "__main__":
+    run()
